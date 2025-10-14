bc325b5e7e685e72f8a0e6e2cbf8f087734aab01
Eric John Piros <ericpiros@yahoo.com>
2025-10-15 01:22:46 +0800

    Manual Miryoku

--- config/miryoku/custom_config.h
+++
@@ -1,7 +0,0 @@
-// Copyright 2021 Manna Harbour
-// https://github.com/manna-harbour/miryoku
-
-#define MIRYOKU_ALPHAS_QWERTY
-#define MIRYOKU_TAP_QWERTY
-#define MIRYOKU_EXTRA_COLEMAKDH
-#define MIRYOKU_KLUDGE_MOUSEKEYSPR
